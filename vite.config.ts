d4fd627d461f1de609ac81380edf51e330717934
Omar Sayed <omarsayed102010@gmail.com>
2026-07-07 14:58:43 +0300

    chore: install project dependencies via node_modules initialization

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     base: './',
+    server: {
+        allowedHosts: ['finalist-stargazer-yesterday.ngrok-free.dev'],
+    },
     build: {
         rollupOptions: {
             maxParallelFileOps: 128,
